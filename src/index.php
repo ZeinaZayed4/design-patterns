ce1e6842ef2a31d096c0d133a0cb05c192795f3f
ZeinaZayed4 <zeinazayed088@gmail.com>
2025-08-05 06:03:18 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,40 +2,23 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\OOP\Abstraction_Encapsulation\BMW;
-use App\OOP\Abstraction_Encapsulation\Employee;
-use App\OOP\Abstraction_Encapsulation\Mercedes;
-use App\OOP\Abstraction_Encapsulation\Microwave;
-use App\OOP\Abstraction_Encapsulation\Order;
-use App\OOP\Inheritance\HTTPClient;
-use App\OOP\Inheritance\TCPClient;
+use App\OOP\Polymorphism\Employee;
+use App\OOP\Polymorphism\OrganizationSalary;
+use App\OOP\Polymorphism\Salary;
 
-$zeina = new Employee('Zeina', 23, 20000);
-$microwave = new Microwave(90, "DE1", 30);
-$bmw = new BMW(320, 2, "Front", "Black");
-$mercedes = new Mercedes(335, 4, "Back", "White");
-$order1 = new Order("Cairo", "Alexandria", 120);
-$order2 = new Order("Suhag", "Aswan", 300);
+$ahmed = new Employee(
+    'Ahmed',
+    30,
+    'Cairo',
+    new OrganizationSalary(
+        10000,
+        0.2,
+        30,
+        2,
+        75,
+        30,
+        0.1
+    )
+);
 
-if ($microwave->turnOn()) {
-    var_dump($microwave->deFreeze());
-    echo '<br />';
-}
-if (!$microwave->turnOff()) {
-    var_dump($microwave->deFreeze());
-    echo '<br />';
-}
-
-var_dump($order1->deliverOrder($bmw)); echo '<br />';
-var_dump($order2->deliverOrder($mercedes)); echo '<br />';
-
-echo '<hr/>';
-
-$http = new HTTPClient('https://www.abc.com', 5);
-echo $http->connect() . '<br/>';
-echo $http->call('/users') . '<br/>';
-echo $http->terminate() . '<br/>';
-echo $http->welcomeAfterConnect() . '<br/>';
-
-$tcp = new TCPClient('https://www.abc.com', 5, 8000);
-echo $tcp->connect() . '<br/>';
+echo "Salary for {$ahmed->getName()} after adding tax is: {$ahmed->getSalary()->calculateSalary()}";
